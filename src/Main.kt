8893087a31f7f1560b98129fc1a2ed71c60e06ef
waimea-lrdiack <lrdiack@waimea.school.nz>
2025-02-25 13:25:15 +1300

    learning mutableLists

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,55 @@
 /**
- * Learning about Kotlin collections, particularly
- * the MutableList type
+ * Learning about Kotlin collections
+ * MutableList type
  */
 
 fun main() {
-    println("Hello World!")
-}
+    println("Kotlin MutableList")
+    println()
 
+    // Create list
+    val snacks = mutableListOf<String>()
+
+    // Add in some things
+    snacks.add("Pringles")
+    snacks.add("Turkish Delight")
+    snacks.add("Cheese")
+    snacks.add("Curly Fries")
+    snacks.add("Ice")
+    snacks.add("Nuclear Bomb")
+    snacks.add("Watermelon")
+
+    //show it
+    println(snacks)
+
+    println(snacks[0])
+    // println(snacks[8]) this would be out of bounds
+
+    // Change a Value
+    snacks[1] = "Fried chicken"
+
+    // remove a value
+    snacks.removeAt(0)
+    println(snacks)
+
+    // add in new items to specific places
+    snacks.add(1, "Pineapple")
+    println(snacks)
+
+    // sort the list
+    snacks.sort()
+    println(snacks)
+
+    // shuffle the list
+    snacks.shuffle()
+    println(snacks)
+
+    // Get a random item from list
+    val item = snacks.random()
+    println(item)
+
+    // Loop through the list
+    for (snack in snacks) {
+        println(snack)
+    }
+}
